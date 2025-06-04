794867908ebeeaf94586691e01789c3a69c7b8e3
Jade Lovelace <jadel@mercury.com>
2025-06-04 16:21:37 -0700

    Address feedback

--- nix/packages/docker-push.nix
+++ nix/packages/docker-push.nix
@@ -15,7 +15,13 @@ writeShellApplication {
     fi
     registry_path=$1
     args=(
-      # insecure-policy: we don't have any signature policy, we are just uploading an image
+      # insecure-policy: we don't have any signature policy, we are just uploading an image.
+      #
+      # We *could* enable signing of docker images, but looking around I've not
+      # seen any signed images elsewhere at the company after a brief look, and
+      # the security model doesn't make sense to me: if you have creds to
+      # upload an image, you surely have the creds to grab the signing key too,
+      # at least in the way we'd implement it.
       --insecure-policy
       copy
       --all
